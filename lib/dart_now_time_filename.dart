c34705a09fb15eec99e80628262120381fb18acb
sunshouhua <sunshouhua@gmail.com>
2019-10-24 13:17:33 +0800

    test code

--- lib/dart_now_time_filename.dart
+++ lib/dart_now_time_filename.dart
@@ -1,7 +1,17 @@
 library dart_now_time_filename;
 
 /// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
+class UtilNowFilename {
+  static String getNowFilename({String prefix: '', String ext: ''}) {
+    final now = DateTime.now();
+    final y = now.year.toString().padLeft(4, '0');
+    final m = now.month.toString().padLeft(2, '0');
+    final d = now.day.toString().padLeft(2, '0');
+    final h = now.hour.toString().padLeft(2, '0');
+    final mi = now.minute.toString().padLeft(2, '0');
+    final s = now.second.toString().padLeft(2, '0');
+    final ml = now.millisecond.toString().padLeft(3, '0');
+    final mc = now.microsecond.toString().padLeft(3, '0');
+    return '$prefix$y$m$d-$h$mi$s-$ml-$mc$ext';
+  }
 }
--- test/dart_now_time_filename_test.dart
+++ test/dart_now_time_filename_test.dart
@@ -3,11 +3,17 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:dart_now_time_filename/dart_now_time_filename.dart';
 
 void main() {
-  test('adds one to input values', () {
-    final calculator = Calculator();
-    expect(calculator.addOne(2), 3);
-    expect(calculator.addOne(-7), -6);
-    expect(calculator.addOne(0), 1);
-    expect(() => calculator.addOne(null), throwsNoSuchMethodError);
-  });
+  // test('adds one to input values', () {
+  //   // final calculator = Calculator();
+  //   // expect(calculator.addOne(2), 3);
+  //   // expect(calculator.addOne(-7), -6);
+  //   // expect(calculator.addOne(0), 1);
+  //   // expect(() => calculator.addOne(null), throwsNoSuchMethodError);
+  //   // expect(UtilNowFilename.getNowFilename(), "aaa");
+  // });
+
+  print(UtilNowFilename.getNowFilename());
+  print(UtilNowFilename.getNowFilename(prefix: 'header-'));
+  print(UtilNowFilename.getNowFilename(ext: '.mp3'));
+  print(UtilNowFilename.getNowFilename(prefix: 'header-', ext: '.mp3'));
 }
