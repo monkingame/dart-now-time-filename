81099a9844f0c48ac46511c8e53fcc44a0fb489d
sunshouhua <sunshouhua@gmail.com>
2019-10-24 18:26:21 +0800

    example

--- example/main.dart
+++ example/main.dart
@@ -1,67 +1,11 @@
 import 'package:dart_now_time_filename/dart_now_time_filename.dart';
-import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
-
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-
-    final filename = NowFilename.genNowFilename(prefix: 'header-', ext: '.mp3');
-    print(filename);
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ),
-    );
-  }
+void main() {
+  print(NowFilename.genNowFilename());
+  print(NowFilename.genNowFilename(prefix: 'header-'));
+  print(NowFilename.genNowFilename(ext: '.mp3'));
+  print(NowFilename.genNowFilename(prefix: 'header-', ext: '.mp3'));
+  print(NowFilename.genNowFilename(
+      prefix: 'header-', ext: '.mp3', seperator: '@'));
+  print(NowFilename.genNowFilename(prefix: null, ext: '.mp3', seperator: null));
 }
